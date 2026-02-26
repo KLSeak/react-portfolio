4b8f5fd5c2b1986fda33d54e27c1fcc383f92abf
Kuzxa <162894285+KLSeak@users.noreply.github.com>
2026-02-26 16:31:54 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
                 <img src="./assets/hand-icon.png" alt="" className="w-6 mb-1" />
             </h3>
             <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">frontend web developer based in Kampong Spue.</h1>
-            <p className="max-w-2xl mx-auto font-Ovo">I am a frontend developer from Kampong Spue, Cambodia with some of experience of Teaching and Sharing !</p>
+            <p className="max-w-2xl mx-auto font-Ovo">I am a Frontend developer from Kampong Spue, Cambodia with some of experience of Teaching and Sharing !</p>
 
             <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
                 <a href="#contact"
